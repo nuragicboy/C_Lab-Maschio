2fd147d9c235d6d58bc393c7f6f2bb0cac257523
Albishh <alberto.carmignani1@gmail.com>
2021-04-21 19:52:37 +0200

    Aggiunto codice per la pulizia dei dati - Problema nella colonna A, leggera riga 19

--- Excel/Clean_Data.py
+++ Excel/Clean_Data.py
@@ -3,18 +3,19 @@ import numpy as np
 
 # Carica il file dalla stessa directory di Python
 df = pd.read_excel('..\Excel\Michelet.xlsx', sheet_name='Sheet1')
-
 # Traspone la tabella convertendola in formato xlsx
 Trans = df.T
 
 Trans.to_excel('Out3.xlsx')
 
-
-
-def Clean_Data():
-    # Copia le prime 17 celle della seconda riga nella terza per eliminare gli spazi vuoti
-    for i in range(0,18,1):
+# Copia le prime 17 celle della seconda riga nella terza per eliminare gli spazi vuoti
+for i in range(0,18,1):
     Trans.iloc[1,i]=Trans.iloc[0,i]
-    Trans.to_excel('Clean1.xlsx')
-    # Eliminare la seconda riga che non serve ad un tubo
+    # Riempe le celle vuote con degli zero
+    T1 = Trans.fillna(value=0.0)
+    # Elimina la seconda riga, ormai fotocopia di quella sotto
+    T1.drop('Codice',inplace=True)
+
+   #NOTA: La colonna A è come se non esistesse, non c'è modo di modificarla. Ste' pensaci tu! XD
+    T1.to_excel('Clean1.xlsx')
 
