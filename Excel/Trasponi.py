c3f73e084d553c362b3f7b91e35f243eac58ab67
nuragicboy <nuragicboy@gmail.com>
2021-04-28 13:30:04 +0200

    aggiunto zoom per scalare la dimensione a pdf img to img.py rimossi i commenti da trasponi

--- Excel/Trasponi.py
+++ Excel/Trasponi.py
@@ -1,20 +1,8 @@
-""""#Aggiungo una riga per permettere l'indicizzazione completa del dataframe
-from openpyxl import load_workbook
-wb = load_workbook('Michelet.xlsx')
-ws = wb.active
-ws.insert_rows(1, amount=1)
-wb.save("Michelet#1.xlsx")"""
-
 import pandas as pd
 
 #Faccio la trasposta del dataframe per posizionare gli articoli sulle righe anzichè sulle colonne
 df = pd.read_excel('Michelet.xlsx', sheet_name='Sheet1')
 trans = df.T
-"""T1 = Trans.fillna(value=0.0)
-# Tutti i valori nulli nella seconda riga vengono sostituiti con i medesimi della riga precedente
-for i in range(0,len(df),1):
-    if T1.iloc[1,i]==0:
-        T1.iloc[1,i]=T1.iloc[0,i]"""
 
 #Elimino la seconda riga
 trans.drop(trans.index[1:2], inplace=True)
--- PDF Immagine to Immagine/PDF Img to Img.py
+++ PDF Immagine to Immagine/PDF Img to Img.py
@@ -2,9 +2,12 @@
 
 import fitz
 
+zoom=3
+
 pdffile = "POLO.pdf"
 doc = fitz.open(pdffile)
 page = doc.loadPage(0)  # number of page
-pix = page.getPixmap()
+mat = fitz.Matrix(zoom, zoom)     #matrice per lo zoom. risoluzione di default 596x842, moltiplicata poi per il numeretto
+pix = page.getPixmap(matrix = mat)
 output = "POLO.jpg"
 pix.writePNG(output)
