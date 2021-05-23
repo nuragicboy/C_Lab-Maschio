9aebda21f2039a0817052e1aa60ca3e4bb501f1c
Albishh <alberto.carmignani1@gmail.com>
2021-05-23 22:35:38 +0200

    Interfaccia grafica per caricare file

--- Interfaccia GUI/Tkinter.py
+++ Interfaccia GUI/Tkinter.py
@@ -1,7 +1,19 @@
-from tkinter import *
-from tkinter.ttk import *
+import numpy as np
+import tkinter as tk
+from tkinter import filedialog
+file=''
 
-ro = Tk()
-w = Label(ro, text="Sono una GUI")
-w.pack()
-ro.mainloop()
+def FileImport():
+    global file
+    file=filedialog.askopenfilename()
+    label=tk.Label(root, text="Selected:"+file).pack()
+
+root=tk.Tk()
+root.title('Main')
+label=tk.Label(root, text="Upload a file:", fg="purple").pack()
+button=tk.Button(root, text='Upload',fg="blue", command=FileImport)
+button.pack()
+
+root.mainloop()
+uploaded_file=np.fromfile(file)
+print(uploaded_file)
