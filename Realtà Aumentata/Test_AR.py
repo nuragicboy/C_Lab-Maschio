aedf46b2b12071c5a5a26527c050bde67e79ffb0
Albishh <alberto.carmignani1@gmail.com>
2021-06-01 18:52:20 +0200

    AR

--- Realtà Aumentata/Test_AR.py
+++ Realtà Aumentata/Test_AR.py
@@ -3,11 +3,8 @@ import cv2
 import numpy as np
 
 cap = cv2.VideoCapture(0)
-imgTarget = cv2.imread('TargetImage.jpg')
+imgTarget = cv2.imread('Targetimage.jpg')
 myVid = cv2.VideoCapture('Video_Test.mp4')
-
-
-
 
 detection = False
 frameCounter = 0
@@ -15,11 +12,12 @@ frameCounter = 0
 success, imgVideo = myVid.read()
 
 hT,wT,cT = imgTarget.shape
-imgVideo = cv2.resize(imgVideo,(wT-150,hT-150))
+imgVideo = cv2.resize(imgVideo,(wT,hT))
 
-orb = cv2.ORB_create(nfeatures=20000)
+orb = cv2.ORB_create(nfeatures=1000)
 kp1, des1 = orb.detectAndCompute(imgTarget, None)
 imgTarget = cv2.drawKeypoints(imgTarget, kp1, None)
+
 
 def stackimages(imgarray,scale,lables=[]):
     sizeW= imgarray[0][0].shape[1]
@@ -68,50 +66,49 @@ while True:
     if not detection:
         myVid.set(cv2.CAP_PROP_POS_FRAMES,0)
         frameCounter = 0
-        else:
+    else:
         if frameCounter == myVid.get(cv2.CAP_PROP_FRAME_COUNT):
             myVid.set(cv2.CAP_PROP_POS_FRAMES, 0)
             frameCounter = 0
         success, imgVideo = myVid.read()
         imgVideo = cv2.resize(imgVideo, (wT, hT))
-
-        matches = bf.knnMatch(des1,des2,k=2)
-        good =[]
-        for m,n in matches:
-            if m.distance < 0.75*n.distance:
-                good.append(m)
-                print(len(good))
-                imgFeatures = cv2.drawMatches(imgTarget, kp1, imgWebcam, kp2, good, None, flags=2)
-                if len(good) > 20:
-                    detection = True
-                    srcPts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
-                    dstPts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
-                    matrix, mask = cv2.findHomography(srcPts,dstPts,cv2.RANSAC,5)
+    bf=cv2.BFMatcher()
+    matches = bf.knnMatch(des1,des2,k=2)
+    good =[]
+    for m,n in matches:
+        if m.distance < 0.75 *n.distance:
+            good.append(m)
+    print(len(good))
+    imgFeatures = cv2.drawMatches(imgTarget, kp1, imgWebcam, kp2, good, None, flags=2)
+    if len(good) > 8:
+        detection = True
+        srcPts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dstPts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        matrix, mask = cv2.findHomography(srcPts,dstPts,cv2.RANSAC,5)
         print(matrix)
 
-        # pts = np.float32([[0,0],[0,hT],[wT,hT],[wT,0]]).reshape(-1,1,2)
-        # dst = cv2.perspectiveTransform(pts,matrix)
-        # img2 = cv2.polylines(imgWebcam,[np.int32(dst)],True,(255,0,255),3)
+        pts = np.float32([[0,0],[0,hT],[wT,hT],[wT,0]]).reshape(-1,1,2)
+        dst = cv2.perspectiveTransform(pts,matrix)
+        img2 = cv2.polylines(imgWebcam,[np.int32(dst)],True,(255,0,255),3)
         #
-        # imgWarp = cv2.warpPerspective(imgVideo,matrix, (imgWebcam.shape[1],imgWebcam.shape[0]))
-        # cv2.imshow('imgWarp', imgWarp)
-        # cv2.waitKey(1)
-        # maskNew = np.zeros((imgWebcam.shape[0],imgWebcam.shape[1]),np.uint8)
-        # cv2.fillPoly(maskNew,[np.int32(dst)],(255,255,255))
-        # maskInv = cv2.bitwise_not(maskNew)
-        # imgAug = cv2.bitwise_and(imgAug,imgAug,mask = maskInv)
-        # imgAug = cv2.bitwise_or(imgWarp,imgAug)
-        #
-        # imgStacked = stackimages(([imgWebcam, imgVideo, imgTarget], [imgFeatures, imgWarp, imgAug]), 0.5)
-    #
-    #     # cv2.imshow(‘maskNew’, imgAug)
-    #     # cv2.imshow(‘imgWarp’, imgWarp)
-    #     # cv2.imshow(‘img2’, img2)
-          #cv2.imshow('imgFeatures', imgFeatures)
-    #     # cv2.imshow(‘ImgTarget’,imgTarget)
-    #     # cv2.imshow(‘myVid’,imgVideo)
-    #     cv2.imshow('Webcam', imgWebcam)
+        imgWarp = cv2.warpPerspective(imgVideo,matrix, (imgWebcam.shape[1],imgWebcam.shape[0]))
 
-            # cv2.imshow('imgStacked', imgStacked)
-            # cv2.waitKey(0)
-            # frameCounter +=1
+        maskNew = np.zeros((imgWebcam.shape[0],imgWebcam.shape[1]),np.uint8)
+        cv2.fillPoly(maskNew,[np.int32(dst)],(255,255,255))
+        maskInv = cv2.bitwise_not(maskNew)
+        imgAug = cv2.bitwise_and(imgAug,imgAug,mask = maskInv)
+        imgAug = cv2.bitwise_or(imgWarp,imgAug)
+        #
+        imgStacked = stackimages(([imgWebcam, imgVideo, imgTarget], [imgFeatures, imgWarp, imgAug]), 0.5)
+    #
+        #cv2.imshow('maskNew', imgAug)
+        #cv2.imshow('imgWarp', imgWarp)
+        #cv2.imshow('img', img2)
+        #cv2.imshow('imgFeatures', imgFeatures)
+        #cv2.imshow('ImgTarget',imgTarget)
+        #cv2.imshow('myVid',imgVideo)
+        #cv2.imshow('Webcam', imgWebcam)
+
+        cv2.imshow('imgStacked', imgStacked)
+        cv2.waitKey(1)
+        frameCounter +=1
